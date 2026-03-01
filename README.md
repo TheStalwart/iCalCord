acd480641b8ed2cc35424c67e4c012cf155886ee
Pavel Shevchuk <stlwrt@gmail.com>
2026-03-01 22:27:17 +0200

    Initial commit. Lots of jank, ngl

---
+++ README.md
@@ -0,0 +1,24 @@
+# iCalCord: Discord Events as iCalendar feeds
+
+A Discord bot to export [Guild Scheduled Events](https://docs.discord.com/developers/resources/guild-scheduled-event#list-scheduled-events-for-guild)
+as industry standard [iCalendar feeds](https://en.wikipedia.org/wiki/ICalendar) (.ics),
+compatible with Google Calendar, Apple iCloud, Microsoft Outlook, Mozilla Thunderbird
+and other sane calendar software.
+
+Production instance: <https://icalcord.retromultiplayer.com/>
+
+Discord API does not return past events,
+so this bot stores them in MongoDB database and inserts into ICS feeds.
+
+## Development environment
+
+Visual Studio Code Dev Container configuration is included,
+and will install required Python dependencies in `postCreateCommand`.
+The choice of Debian Bookworm as a container image is intentional,
+as it allows to install and run MongoDB and memcached inside DevContainer.
+Open the `devcontainer.json` and follow instructions in comments.
+
+1. Create New Application in [Discord Developer Portal](https://discord.com/developers/applications/)
+1. Create a MongoDB database with a collection "events"
+1. Run memcached instance
+1. Copy `config.yaml.example` to `config.yaml` and fill in values.
---
+++ bot.py
@@ -0,0 +1,350 @@
+from aiohttp import web
+from datetime import datetime, timezone
+from icalendar import Calendar, Event, FreeBusy
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+from rich import print
+from rich.pretty import pprint
+import argparse
+import asyncio
+import discord
+import memcache
+import os
+import pathlib
+import requests
+import sys
+import yaml
+
+
+# Define file paths
+PROJECT_ROOT = pathlib.Path(__file__).parent.resolve()
+CONFIG_FILE_PATH = os.path.join(PROJECT_ROOT, "config.yaml")
+FRONTEND_ROOT_PATH = os.path.join(PROJECT_ROOT, "frontend")
+FRONTEND_STATIC_PATH = os.path.join(FRONTEND_ROOT_PATH, "static")
+
+
+# Build ArgumentParser https://docs.python.org/3/library/argparse.html
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument(
+    "--debug",
+    action="store_true",
+    help="Print extra values, including Discord Bot Token, and generate static .ics files",
+)
+args = arg_parser.parse_args()
+
+
+config = {}
+with open(CONFIG_FILE_PATH) as stream:
+    try:
+        config = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(
+            f"[red]Error:[/red] Could not read config file at [yellow]{CONFIG_FILE_PATH}[/yellow]!"
+        )
+        print(exc)
+        sys.exit(1)
+
+
+# Create MongoDB connection
+mongo_client = MongoClient(config["mongodb"]["url"], retryWrites=True)
+mongo_db = mongo_client.get_default_database()
+mongo_collection = mongo_db.events
+
+
+# Create Memcached client
+memcache_client = memcache.Client([config["memcache"]["url"]], debug=0)
+
+
+def memcache_key_for_guild_events(guild_id):
+    return f"{config['memcache']['key_prefix']}_guild_events_{guild_id}"
+
+
+def retrieve_memcached_current_events_for_guild(guild_id):
+    url = f"https://discord.com/api/v10/guilds/{guild_id}/scheduled-events"
+    memcache_key = memcache_key_for_guild_events(guild_id)
+
+    cached_response = memcache_client.get(memcache_key)
+    if cached_response:
+        print(
+            f"Using cached response for lurkable guild ID: [yellow]{guild_id}[/yellow]"
+        )
+        return cached_response
+
+    headers = {"Authorization": f"Bot {config['discord']['token']}"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        response_json = response.json()
+        pprint(response_json)
+        memcache_client.set(
+            memcache_key, response_json, time=config["memcache"]["value_ttl_seconds"]
+        )
+        return response_json
+
+    print(
+        f"[red]Error:[/red] Failed to fetch Discord events for lurkable Guild [yellow]{guild_id}[/yellow]: {response.content}"
+    )
+    return None
+
+
+def upsert_event(event_data: dict):
+    # Fields that indicate a meaningful change to the event.
+    # Discord API output includes fields that may change
+    # but not impact ICS output, e.g. creator.accent_color.
+    MEANINGFUL_FIELDS = [
+        "name",
+        "description",
+        "scheduled_start_time",
+        "scheduled_end_time",
+        "entity_metadata",  # .location
+    ]
+
+    try:
+        existing = mongo_collection.find_one({"id": event_data["id"]})
+
+        # Check if any of the meaningful fields changed
+        data_changed = existing is None or any(
+            event_data.get(field) != existing.get(field) for field in MEANINGFUL_FIELDS
+        )
+
+        event_update = {
+            "$set": {
+                **event_data,
+                "icalcord_last_seen_time": datetime.now(timezone.utc),
+                # Convert ISO 8601 strings to datetime objects
+                "icalcord_scheduled_end_time": datetime.fromisoformat(
+                    event_data["scheduled_end_time"]
+                ),
+                "icalcord_scheduled_start_time": datetime.fromisoformat(
+                    event_data["scheduled_start_time"]
+                ),
+            },
+            "$setOnInsert": {"icalcord_discovered_time": datetime.now(timezone.utc)},
+        }
+
+        # Only set updated time if something meaningful changed
+        if data_changed:
+            print(
+                f"Meaningful update for event [magenta]{event_data['name']}[/magenta]"
+            )
+            event_update["$set"]["icalcord_updated_time"] = datetime.now(timezone.utc)
+
+        result = mongo_collection.update_one(
+            {"id": event_data["id"]},
+            event_update,
+            upsert=True,
+        )
+
+        if result.upserted_id:
+            print(
+                f"Inserted new event with _id: [yellow]{result.upserted_id}[/yellow] ([magenta]{event_data['name']}[/magenta])"
+            )
+        elif result.modified_count:
+            print(f"Updated existing event ([magenta]{event_data['name']}[/magenta])")
+        else:
+            print("No changes needed")
+
+    except PyMongoError as e:
+        print("[red]Database error:[/red]", e)
+        raise
+
+
+def regenerate_static_ics_file(guild_id):
+    if not args.debug:
+        print(
+            f"Skipping regeneration of .ics file for guild ID: [yellow]{guild_id}[/yellow] (debug mode disabled)"
+        )
+        return
+
+    print(f"Regenerating .ics file for guild ID: [yellow]{guild_id}[/yellow]")
+    ics_path = os.path.join(FRONTEND_STATIC_PATH, f"{guild_id}.ics")
+    events = mongo_collection.find({"guild_id": guild_id})
+
+    # The icalendar library https://icalendar.readthedocs.io/en/stable/index.html
+    # is fairly low level and can generate output that is not spec-compliant,
+    # make sure to validate edge cases with https://icalendar.org/validator.html
+    ics = Calendar()
+    ics.prodid = "-//icalcord.retromultiplayer.com//iCalCord//EN"
+    ics.version = "2.0"
+    ics.calendar_name = f"Guild {guild_id} Events"
+    for event in events:
+        ics_event = Event()
+        ics_event.uid = f"{event['id']}@icalcord"
+        ics_event.summary = event["name"]
+        ics_event.stamp = event.get("icalcord_updated_time")
+
+        # I'm not gonna throw away old event data (right now)
+        if event.get("icalcord_scheduled_start_time"):
+            ics_event.start = event["icalcord_scheduled_start_time"]
+        else:
+            if isinstance(event["scheduled_start_time"], datetime):
+                ics_event.start = event["scheduled_start_time"]
+            else:
+                ics_event.start = datetime.fromisoformat(event["scheduled_start_time"])
+
+        if event.get("icalcord_scheduled_end_time"):
+            ics_event.end = event["icalcord_scheduled_end_time"]
+        else:
+            if isinstance(event["scheduled_end_time"], datetime):
+                ics_event.end = event["scheduled_end_time"]
+            else:
+                ics_event.end = datetime.fromisoformat(event["scheduled_end_time"])
+
+        if event.get("description"):
+            ics_event.description = event["description"]
+
+        if event.get("entity_metadata") and event["entity_metadata"].get("location"):
+            ics_event.location = event["entity_metadata"]["location"]
+
+        ics.add_component(ics_event)
+
+    if events.retrieved == 0:
+        print(
+            f"No events found for guild ID: [yellow]{guild_id}[/yellow], inserting VFREEBUSY component for spec compliance"
+        )
+        # Insert a VFREEBUSY component for spec compliance
+        # when we don't have any events to insert at the time of feed generation.
+        # This ensures that calendar clients will recognize the feed as valid
+        # and continue to check for updates,
+        # rather than discarding it as an invalid source.
+        vfreebusy = FreeBusy()
+        vfreebusy.uid = f"vfreebusy-{guild_id}@icalcord"
+        # Arbitrary fixed date,
+        # not too far in the past to avoid client rejections,
+        # not too far in the future to avoid confusion
+        vfreebusy.stamp = datetime(2026, 3, 1, tzinfo=timezone.utc)
+        ics.add_component(vfreebusy)
+
+    with open(ics_path, "wb") as ics_file:
+        ics_file.write(ics.to_ical())
+
+    print(f"Saved .ics file to: [yellow]{ics_path}[/yellow]")
+
+
+# Set up Discord API client
+discord_intents = discord.Intents.default()
+discord_client = discord.Client(intents=discord_intents)
+
+
+async def frontend_index(request):
+    return web.FileResponse(os.path.join(FRONTEND_ROOT_PATH, "index.html"))
+
+
+async def endpoint_handler_ics_feed_generator(request):
+    guild_id = request.match_info["guild_id"]
+
+    # Basic validation to ensure guild_id is a plausible Discord Snowflake.
+    if not guild_id.isdigit() or not (16 <= len(guild_id) <= 20):
+        return web.json_response({"error": "Invalid guild_id format"}, status=400)
+
+    # TODO: serve from memory, because static files are only generated when debugging
+    await fetch_and_store_events_for_guild(guild_id)
+    return web.FileResponse(
+        os.path.join(FRONTEND_STATIC_PATH, f"{guild_id}.ics"),
+        headers={"Content-Type": "text/calendar; charset=utf-8"},
+    )
+    # switch to this to serve from memory
+    # return web.Response(
+    #     body=ics_content,
+    #     content_type="text/calendar",
+    #     charset="utf-8"
+    # )
+
+
+async def start_http_server():
+    app = web.Application()
+
+    # API endpoint
+    app.router.add_get("/feed/{guild_id}.ics", endpoint_handler_ics_feed_generator)
+
+    # Root index.html
+    app.router.add_get("/", frontend_index)
+
+    # Serve entire /frontend/static directory
+    app.router.add_static("/static/", path=FRONTEND_STATIC_PATH)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    site = web.TCPSite(runner, config["frontend"]["ip"], config["frontend"]["port"])
+    await site.start()
+
+
+async def fetch_and_store_events_for_guild(guild_id):
+    print(f"Fetching events for guild ID: [yellow]{guild_id}[/yellow]")
+
+    # The bot doesn't need to be present on server to fetch events
+    # if the server is "lurkable"
+    # https://docs.discord.com/developers/resources/guild-scheduled-event#list-scheduled-events-for-guild
+    lurkable_events_json = retrieve_memcached_current_events_for_guild(guild_id)
+    if lurkable_events_json is not None:
+        for event_json in lurkable_events_json:
+            upsert_event(event_json)
+        regenerate_static_ics_file(guild_id)
+        return
+
+    print(
+        f"Attempting to fetch events using discord.py library for guild ID: [yellow]{guild_id}[/yellow]"
+    )
+    memcache_key = memcache_key_for_guild_events(guild_id)
+    cached_response = memcache_client.get(memcache_key)
+    if cached_response is not None:
+        print(
+            f"Using cached discord.py response for guild ID: [yellow]{guild_id}[/yellow]"
+        )
+        pprint(cached_response)
+        regenerate_static_ics_file(guild_id)
+        return
+
+    guild = discord_client.get_guild(guild_id)
+    if guild is None:
+        print(
+            f"[red]Error:[/red] Guild [yellow]{guild_id}[/yellow] not found (bot not invited?)"
+        )
+        return
+    events = await guild.fetch_scheduled_events()
+    if not events:
+        print("No scheduled events found.")
+    else:
+        print(f"Found [green]{len(events)}[/green] scheduled events:")
+        pprint(events)
+        # for ev in events:
+        #     print(f"Event: {ev.name}")
+        #     print(f"  ID: {ev.id}")
+        #     print(f"  Start: {ev.start_time}")
+        #     print(f"  End:   {ev.end_time}")
+        #     print(f"  Description: {ev.description!r}")
+        #     print("-" * 40)
+    memcache_client.set(
+        memcache_key, events, time=config["memcache"]["value_ttl_seconds"]
+    )
+
+    # TODO: Delete upcoming events that are absent from API response?
+    # Is there a more reliable way to detect deleted events than relying on API response consistency?
+    regenerate_static_ics_file(guild_id)
+
+
+@discord_client.event
+async def on_ready():
+    print(f"We have logged in as [yellow]{discord_client.user}[/yellow]")
+    await start_http_server()
+    print("Started HTTP Server")
+
+
+# TODO: implement a slash command to respond with .ics feed URL for the server, e.g. /icalcord
+@discord_client.event
+async def on_message(message):
+    if message.author == discord_client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+
+async def main():
+    await discord_client.start(config["discord"]["token"])
+
+
+if __name__ == "__main__":
+    if args.debug:
+        print(f"Discord Bot Token: [yellow]{config['discord']['token']}[/yellow]")
+    asyncio.run(main())
